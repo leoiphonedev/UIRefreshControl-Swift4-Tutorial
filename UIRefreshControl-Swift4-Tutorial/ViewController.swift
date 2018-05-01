9ef1bff4ad8febae90b1580ec3c1e7960eb8330c
leoiphonedev <aggarwalaman1985@gmail.com>
2018-05-01 21:41:57 +0530

    Initial Commit

---
+++ UIRefreshControl-Swift4-Tutorial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UIRefreshControl-Swift4-Tutorial
+//
+//  Created by Aman Aggarwal on 01/05/18.
+//  Copyright © 2018 Aman Aggarwal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
